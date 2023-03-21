38bfb54b0b63d94692b9202cea567cffc5e88f41
mcekic03 <ceka.mihajlo03@gmail.com>
2023-03-21 18:06:33 +0100

    finis?

--- resources/js/artikalklasa.js
+++ resources/js/artikalklasa.js
@@ -212,6 +212,139 @@ nizartikli.push(artikal59);
 const artikal60 = new Artikal([1550],'Paris 03',['black'],['1/2','3/4'],'Samodrzeca carapa od 20 dena bogatom cipkom I dupli silicon. Sastav: 80% poliamid 20%','Samodrzece carape')
 nizartikli.push(artikal60);
 
+const artikal61 = new Artikal([1190],'Sumer 8',['black'],['1/2','3/4'],'Samodrzeca carapa fina ekstra tanka od 8 dena ','Samodrzece carape')
+nizartikli.push(artikal61);
+
+const artikal62 = new Artikal([1490],'Coco N55',['black'],['1/2','3/4'],'Samostojeće čarape u obliku mrežice s lurexom . Imaju lastiž bez čipke od 7 cm sa silikonom i nevidljivo ojačanje na nožnim prstima. Lepo svjetlucaju na nozi i dodaju stila.Sastav: 90% poliamid, 10% elastan','Samodrzece carape')
+nizartikli.push(artikal62);
+
+const artikal63 = new Artikal([1790],'Coco W15',['black'],['1/2','3/4'],'Samodrzeca carapa microfiber od 40 dena.Sastav 92%poliamid i 8% elastin','Samodrzece carape')
+nizartikli.push(artikal63);
+
+const artikal64 = new Artikal([1790],'Coco X19',['black'],['1/2','3/4'],'Samodrzeca mrezasta carapa sa tackicama i sa cipkom.Sastav 81%poliamid i 19% elastin','Samodrzece carape')
+nizartikli.push(artikal64);
+
+const artikal65 = new Artikal([1790],'Coco X20',['black'],['1/2','3/4'],'Klasicne samodrzece carape sa cipkom debljina 20 dena.Sastav 90% poliamid i 10%elastin','Samodrzece carape')
+nizartikli.push(artikal65);
+
+const artikal66 = new Artikal([1190],'Coco Vena',['black'],['1/2','3/4'],'Samostojeće čarape 15 den. Bez vidljivog ojačanja prstiju s elegantnim silikonskim završnim slojem od 8 cm.Sastav: 84% poliamid, 16% elastan','Samodrzece carape')
+nizartikli.push(artikal66);
+
+const artikal67 = new Artikal([1190],'Coco B21',['black'],['1/2','3/4'],'Samodrzeca carapa bez cipke sa imitacijom tetovaye na jednoj nozi.','Samodrzece carape')
+nizartikli.push(artikal67);
+
+const artikal69 = new Artikal([1490],'Coco 718',['black'],['1/2','3/4'],'Samodrzeca carapa sa imitacijom haltera.','Samodrzece carape')
+nizartikli.push(artikal69);
+
+const artikal70 = new Artikal([1490],'Coco 718',['black'],['1/2','3/4'],'Samodrzeca carapa sa imitacijom haltera.','Samodrzece carape')
+nizartikli.push(artikal70);
+
+const artikal71 = new Artikal([2470],'Coco 516',['black'],['1/2','3/4'],'Samodrzeca carapa sa pertlanjem pri vrhu zadnje strane.','Samodrzece carape')
+nizartikli.push(artikal71);
+
+const artikal72 = new Artikal([1390],'Chanse 100',['nero'],['1/2','3/4'],'Samodrzece carape sa cipkom od 100 dena neprovidne.','Samodrzece carape')
+nizartikli.push(artikal72);
+
+const artikal73 = new Artikal([1790],'Desire L08',['black'],['1/2','3/4'],'Samodrzeca carapa sa imitacijom haltera na butini.','Samodrzece carape')
+nizartikli.push(artikal73);
+
+const artikal74 = new Artikal([1190],'Coco I01',['visone'],['1/2','3/4'],'Samodrzeca carapa boja koze sa crnim tackicama.','Samodrzece carape')
+nizartikli.push(artikal74);
+
+const artikal75 = new Artikal([1690],'Coco T25',['black'],['1/2','3/4'],'Samodrzeca carapa sa prelepom cipkom I duplim silikonom od 20 dena. Sastav: 89% poliamid, 10% elastane, 1% pamuk','Samodrzece carape')
+nizartikli.push(artikal75);
+
+const artikal76 = new Artikal([650],'Akte',['black','glace','visone','bianco'],['1/2','3/4'],'Carapa za halter od 15 dena. Sastav: 80% poliamid, 20% elastan','Carape za halter')
+nizartikli.push(artikal76);
+
+const artikal77 = new Artikal([1550],'Paris 04',['black'],['1/2','3/4'],'Carapa za halter sa cipkom od 20 dena. Sastav: 84% poliamid, 16% elastan','Carape za halter')
+nizartikli.push(artikal77);
+
+const artikal78 = new Artikal([1390],'Cherry Jet',['black'],['1/2','3/4'],'Komplet carape i halter sa cirkonima.','Carape za halter')
+nizartikli.push(artikal78);
+
+const artikal79 = new Artikal([2290,2290],'C-407',['black'],['S','M','L','XL'],'Ekskluzivni zenski ves komplet korset-gacice','Zenski ves')
+nizartikli.push(artikal79);
+
+const artikal80 = new Artikal([1490,1490],'B-204',['black'],['S','M','L','XL'],'Ekskluzivni ves cipkasti bodi','Zenski ves')
+nizartikli.push(artikal80);
+
+const artikal80c = new Artikal([1490,1490],'B-206',['black'],['S','M','L','XL'],'Ekskluzivan zenski bodi sa trakama ','Zenski ves')
+nizartikli.push(artikal80c);
+
+const artikal81 = new Artikal([1490,1490],'B-209',['black'],['S','M','L','XL'],'Zenska ves spavacica kratka prozirna','Zenski ves')
+nizartikli.push(artikal81);
+
+const artikal82 = new Artikal([1490,1490],'B-216',['pink'],['S','M','L','XL'],'Ekskluzivan roze bodi sa crnim trakama','Zenski ves')
+nizartikli.push(artikal82);
+
+const artikal83 = new Artikal([1490,1490],'B-218',['gray'],['S','M','L','XL'],'Ekskluzivni ves cipkasti bodi ','Zenski ves')
+nizartikli.push(artikal83);
+
+const artikal84 = new Artikal([1490,1490],'V-506',['white'],['S','M','L','XL'],'Bela zenska spavacica sa halterima i krznom oko vrata','Zenski ves')
+nizartikli.push(artikal84);
+
+const artikal85 = new Artikal([1490,1490],'D-302',['black'],['S','M','L','XL'],'Zenska spavacica sa jednom bratelom','Zenski ves')
+nizartikli.push(artikal85);
+
+const artikal86 = new Artikal([3720,3720],'U-815',['black'],['S','M','L','XL'],'Zenska ves dugacka spavacica prozirna sa plavim vezom na brushalteru','Zenski ves')
+nizartikli.push(artikal86);
+
+const artikal87 = new Artikal([3990,3990],'U-816',['black'],['S','M','L','XL'],'Zenska ves kraca spavacica prozirna sa plavim vezom na brushalteru','Zenski ves')
+nizartikli.push(artikal87);
+
+const artikal88 = new Artikal([1490,1490],'U-818',['black'],['S','M','L','XL'],'Kraca crna spavacica sa ukrstenim trakama i cipkom pri dnu','Zenski ves')
+nizartikli.push(artikal88);
+
+const artikal89 = new Artikal([1190],'Brus na lepljenje',['black','natural'],['A','B','C','D'],'Zenski brus bez bratele i bez zadnjeg kopcanja sa unutrasnje strane je oblozen silikonom.','Zenski ves')
+nizartikli.push(artikal89);
+
+const artikal90 = new Artikal([1190],'Brus B',['black','natural','red','white'],['75B','80B','85B'],'Push up brushalter koji moze da se nosi sa i bez bratela. Dobijaju se bratele i traka  u  boji brusa i providna.Zadnja traka na brushalteru se podesava kao I bratela tako da kod ovog modela obim nije problem.','Zenski ves')
+nizartikli.push(artikal90);
+
+const artikal91 = new Artikal([1190],'Brus C',['black','natural','red','white'],['75B','80B','85B'],'Push up brushalter koji moze da se nosi sa i bez bratela. Dobijaju se bratele sa cirkonima i traka  u  boji brusa i providna.Zadnja traka na brushalteru se podesava kao i bratela tako da kod ovog modela obim nije problem.','Zenski ves')
+nizartikli.push(artikal91);
+
+const artikal92 = new Artikal([1190],'Brus B2',['black','natural','red','white'],['75B','80B','85B'],'Elegantni push up brushalter koji moze da se nosi sa i bez bratela.','Zenski ves')
+nizartikli.push(artikal92);
+
+const artikal93 = new Artikal([1790],'Brus ABCD',['black','natural'],['A','B','C','D'],'Brushalter bez savova i bez bratela.','Zenski ves')
+nizartikli.push(artikal93);
+
+const artikal94 = new Artikal([1190],'Brus B3',['black','natural','red','white'],['75B','80B','85B'],'Brushalter push up sa cipkom ispod korpe','Zenski ves')
+nizartikli.push(artikal94);
+
+const artikal95 = new Artikal([1690],'M2',['black','natural'],['A','B','C'],'Besavni dublji mider izuzetnog hvaliteta','Zenski ves')
+nizartikli.push(artikal95);
+
+const artikal96 = new Artikal([1200,1200],'M1',['black','natural'],['M','L','XL'],'Besavni mider','Zenski ves')
+nizartikli.push(artikal96);
+
+const artikal97 = new Artikal([1200],'M3',['black','natural'],['U'],'Mider sa nogavicama sa fiksatorima za stomak zadnjicu i butine','Zenski ves')
+nizartikli.push(artikal97);
+
+const artikal98 = new Artikal([420,420],'G1',['black','natural','red','white'],['L','XL','2XL'],'Zenske besavne tange izuzetnog hvaliteta','Zenski ves')
+nizartikli.push(artikal98);
+
+const artikal99 = new Artikal([420,420],'G2',['black','natural','red','white'],['L','XL','2XL'],'Zenske besavne brazilke izuzetnog hvaliteta','Zenski ves')
+nizartikli.push(artikal99);
+
+const artikal100 = new Artikal([420,420],'G3',['black','natural','red','white'],['L','XL','2XL','3XL'],'Zenske besavne gace','Zenski ves')
+nizartikli.push(artikal100);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
